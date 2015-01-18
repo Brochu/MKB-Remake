e51b1c0d0ab0d92e4b2489bd1c42ff8bcb071933
Brochu <alexbrochu1@gmail.com>
2015-01-18 17:04:00 -0500

    Ajout d'un script pour gerer la rotation d'un stage base sur les axes d'un joystick.

---
+++ proj/Assets/Scripts/StageRotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageRotator : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Stage Rotator script started.");
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        string asdf = string.Format("x: {0}; \ny: {1}", x, y);
+
+        Debug.Log(asdf);
+    }
+}
