85e1ec5e8a37341f308a36a5628cf9d28fc2c04e
Brochu <alexbrochu1@gmail.com>
2015-01-31 23:17:59 -0500

    Ajout d'un script pour traiter le mouvement d'une camera qui suit un objet donne

---
+++ proj/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+
+    public GameObject objectToFollow = null;
+    private Vector3 lastObjectPos = Vector3.zero;
+
+    void Start () {
+        if (!objectToFollow)
+        {
+            Debug.Log("FollowCam: Pas d'objet a suivre ... desactivation !");
+            this.enabled = false;
+        }
+        lastObjectPos = objectToFollow.transform.position;
+    }
+    
+    void LateUpdate () {
+        // Appliquer le mouvement
+        lastObjectPos += Vector3.zero;
+
+        lastObjectPos = objectToFollow.transform.position;
+    }
+}
