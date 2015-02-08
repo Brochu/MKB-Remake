9d498b5a788a18d9f96d77c85a477c4b0c7ecb18
Brochu <alexbrochu1@gmail.com>
2015-02-07 20:02:29 -0500

    enlever le builds folder (gitignore), et ajout de certain meta files

---
+++ proj/Assets/Scripts/RotatingObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingObject : MonoBehaviour {
+
+    public GameObject obj = null;
+    public float speed = 0.0f;
+    public float force = 0.0f;
+
+    private Transform objTansform = null;
+    private float currentYRot = 0.0f;
+
+    void Start () {
+
+        if (obj == null)
+        {
+            Debug.Log("NO OBJECT FOUND!");
+            this.enabled = false;
+        }
+
+        objTansform = obj.transform;
+        currentYRot = objTansform.rotation.y;
+    }
+    
+    void Update () {
+        currentYRot = (currentYRot + speed) % 360;
+
+        Quaternion currentRot = Quaternion.Euler(0, currentYRot, 0);
+        objTansform.rotation = currentRot;
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        Transform pachinko = objTansform;
+        Transform bumpee = c.gameObject.transform;
+
+        Vector3 dir = (bumpee.position - pachinko.position).normalized;
+        dir *= force;
+
+        c.rigidbody.AddForce(dir, ForceMode.Impulse);
+    }
+}
