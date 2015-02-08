8d1d15040c2ebfbb6a7977d2ccb2092832b77b1f
Brochu <alexbrochu1@gmail.com>
2015-02-08 00:09:27 -0500

    Ajout d'un script pour gerer les objs du type de la banade dans MKB original

---
+++ proj/Assets/Scripts/Banana.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Banana : MonoBehaviour {
+
+    public float spinSpeed = 0.0f;
+
+    private Transform t = null;
+    private float currentYRot = 0.0f;
+    private bool pickupable = true;
+
+    void Start () {
+        t = gameObject.transform;
+    }
+    
+    void Update () {
+        currentYRot = (currentYRot + spinSpeed) % 360;
+        t.rotation = Quaternion.Euler(0.0f, currentYRot, 0.0f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!pickupable)
+            return;
+
+        pickupable = false;
+
+        // update banana count
+        StartCoroutine(pickup());
+    }
+
+    IEnumerator pickup()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            t.localScale = t.localScale * 0.7f;
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
