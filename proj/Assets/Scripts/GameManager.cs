e71f2810cb9de8614292ed66ef680c30d1e6071f
Brochu <alexbrochu1@gmail.com>
2015-03-16 01:48:52 -0400

    Ajout d'un script pour gerer les valeurs du jeu (temps, score, nb de bananes ...)

---
+++ proj/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public float time;
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Stage is started");
+        StartCoroutine(stageStartTimer());
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    IEnumerator stageStartTimer()
+    {
+        Debug.Log("Starting countdown in ...");
+        for (int i = 5; i > 0; i--)
+        {
+            Debug.Log(i);
+            yield return new WaitForSeconds(1.0f);
+        }
+        Debug.Log("GO !");
+
+        while (time > 0)
+        {
+            Debug.Log(time + " sec");
+            yield return new WaitForSeconds(0.1f);
+            time -= 0.1f;
+        }
+
+        Debug.Log("Time over !");
+    }
+}
