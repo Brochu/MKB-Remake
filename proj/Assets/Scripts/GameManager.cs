9c74ac66955960c8f6392221d457aaa0a8da9128
Brochu <alexbrochu1@gmail.com>
2015-03-16 02:07:40 -0400

    GameManager tient compte du temps qui passe et du nombre de bananes obtenues. On affiche les info dans un menu temporaire

--- proj/Assets/Scripts/GameManager.cs
+++ proj/Assets/Scripts/GameManager.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System;
 
 public class GameManager : MonoBehaviour {
 
-    public float time;
+    public float time = 0.0f;
+    public int bananaCount = 0;
+    public Text timeTxt = null;
+    public Text bananaTxt = null;
+
+    private string timeStr = "Time : ";
+    private string bananaStr = "Ohhhh Banana : ";
+    private int maxBananaCount = 100;
+
     // Use this for initialization
     void Start () {
         Debug.Log("Stage is started");
@@ -12,11 +22,28 @@ public class GameManager : MonoBehaviour {
     
     // Update is called once per frame
     void Update () {
-    
+        if (Input.GetKey(KeyCode.B)) addBanana();
+    }
+
+    public void updateTime()
+    {
+        timeTxt.text = timeStr + string.Format("{0:f1}", time);
+    }
+
+    public void updateBananaCount()
+    {
+        bananaTxt.text = bananaStr + bananaCount.ToString();
+    }
+
+    private void addBanana()
+    {
+        bananaCount = (bananaCount + 1) % maxBananaCount;
+        updateBananaCount();
     }
 
     IEnumerator stageStartTimer()
     {
+        updateTime();
         Debug.Log("Starting countdown in ...");
         for (int i = 5; i > 0; i--)
         {
@@ -27,7 +54,7 @@ public class GameManager : MonoBehaviour {
 
         while (time > 0)
         {
-            Debug.Log(time + " sec");
+            updateTime();
             yield return new WaitForSeconds(0.1f);
             time -= 0.1f;
         }
