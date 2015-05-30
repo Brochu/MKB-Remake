e706b673525bad04b35ae6c6187b834a64b11596
Brochu <alexbrochu1@gmail.com>
2015-05-30 16:41:54 -0400

    added events to signal start of stage, and removed controls to player at end of stage (time over)

--- proj/Assets/Scripts/GameManager.cs
+++ proj/Assets/Scripts/GameManager.cs
@@ -5,10 +5,18 @@ using System;
 
 public class GameManager : MonoBehaviour {
 
+    public GameObject playerObj = null;
+    public Camera playerCam = null;
+    public Camera introCam = null;
+
     public float time = 0.0f;
     public int bananaCount = 0;
     public Text timeTxt = null;
     public Text bananaTxt = null;
+
+    private IntroCutsceneController introController = null;
+    private PlayerController player = null;
+    private Rigidbody playerPhysics = null;
 
     private string timeStr = "Time : ";
     private string bananaStr = "Ohhhh Banana : ";
@@ -16,13 +24,37 @@ public class GameManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        // Get player controller object
+        if (playerObj == null)
+        {
+            Debug.Log("No player object given ... disabling CutsceneController");
+            this.enabled = false;
+            return;
+        }
+        player = playerObj.GetComponent<PlayerController>();
+        playerPhysics = playerObj.GetComponent<Rigidbody>();
+
+        if (player == null || playerPhysics == null)
+        {
+            Debug.Log("No player controller object found ... disabling CutsceneController");
+            this.enabled = false;
+            return;
+        }
+
+        introController = introCam.GetComponent<IntroCutsceneController>();
+        introController.IntroDone += startStage;
+
         Debug.Log("Stage is started");
-        StartCoroutine(stageStartTimer());
+        startCutscene();
     }
     
     // Update is called once per frame
     void Update () {
-        if (Input.GetKey(KeyCode.B)) addBanana();
+    }
+
+    void OnDestroy()
+    {
+        introController.IntroDone -= startStage;
     }
 
     public void updateTime()
@@ -35,14 +67,42 @@ public class GameManager : MonoBehaviour {
         bananaTxt.text = bananaStr + bananaCount.ToString();
     }
 
+    private void startCutscene()
+    {
+        // Switch to the cutscene camera and disable inputs
+        introCam.enabled = true;
+        playerCam.enabled = false;
+
+        player.ignoreInputs = true;
+        playerPhysics.useGravity = false;
+    }
+
     private void addBanana()
     {
         bananaCount = (bananaCount + 1) % maxBananaCount;
         updateBananaCount();
     }
 
+    private void startStage()
+    {
+        playerPhysics.useGravity = true;
+        StartCoroutine(givePlayerControls());
+    }
+
+    IEnumerator givePlayerControls()
+    {
+        yield return new WaitForSeconds(1);
+        introCam.enabled = false;
+        playerCam.enabled = true;
+        
+        player.ignoreInputs = false;
+
+        StartCoroutine(stageStartTimer());
+    }
+
     IEnumerator stageStartTimer()
     {
+        /*
         updateTime();
         Debug.Log("Starting countdown in ...");
         for (int i = 5; i > 0; i--)
@@ -51,14 +111,18 @@ public class GameManager : MonoBehaviour {
             yield return new WaitForSeconds(1.0f);
         }
         Debug.Log("GO !");
+        */
 
-        while (time > 0)
+        while (time >= 0)
         {
             updateTime();
             yield return new WaitForSeconds(0.1f);
             time -= 0.1f;
         }
 
+        // TODO: add event to signal stage failed
         Debug.Log("Time over !");
+        player.ignoreInputs = true;
+        playerPhysics.Sleep();
     }
 }
--- proj/Assets/Scripts/IntroCutsceneController.cs
+++ proj/Assets/Scripts/IntroCutsceneController.cs
@@ -1,59 +1,22 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class IntroCutsceneController : MonoBehaviour {
-    public GameObject playerObj = null;
-    public Camera playerCam = null;
-    public Camera introCam = null;
-
-    PlayerController player = null;
-    Rigidbody playerPhysics = null;
+    [HideInInspector]
+    public event Action IntroDone;
 
     // Use this for initialization
     void Start () {
-        // Get player controller object
-        if (playerObj == null)
-        {
-            Debug.Log("No player object given ... disabling CutsceneController");
-            this.enabled = false;
-            return;
-        }
-        player = playerObj.GetComponent<PlayerController>();
-        playerPhysics = playerObj.GetComponent<Rigidbody>();
-
-        if (player == null || playerPhysics == null)
-        {
-            Debug.Log("No player controller object found ... disabling CutsceneController");
-            this.enabled = false;
-            return;
-        }
-
-        // Switch to the cutscene camera and disable inputs
-        introCam.enabled = true;
-        playerCam.enabled = false;
-
-        player.ignoreInputs = true;
-        playerPhysics.useGravity = false;
     }
     
     // Update is called once per frame
     void Update () {
-    
     }
 
     public void animationDone(float time)
     {
         // End of the intro cutscene giving control to player
-        playerPhysics.useGravity = true;
-        StartCoroutine(givePlayerControls());
-    }
-
-    IEnumerator givePlayerControls()
-    {
-        yield return new WaitForSeconds(1);
-        introCam.enabled = false;
-        playerCam.enabled = true;
-        
-        player.ignoreInputs = false;
+        IntroDone();
     }
 }
